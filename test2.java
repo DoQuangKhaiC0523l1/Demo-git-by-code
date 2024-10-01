1b46d6c73fc5d45103aad5747bbe21b176696568
DoQuangKhaiC0523l1 <dok962597@gmail.com>
2024-10-01 19:59:54 +0700

    Changes code

--- test2.java
+++ test2.java
@@ -3,5 +3,9 @@ package org.example;
 public class test2 {
     public static void main(String[] args) {
         System.out.println("Hello world");
+        System.out.println("Hello world");
+        System.out.println("Hello world");
+        System.out.println("Hello world");
+        System.out.println("Hello world");
     }
 }
